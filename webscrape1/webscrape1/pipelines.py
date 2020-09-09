6482e23ea66302982e82254d54c1f8277afa4645
asaw4 <asthaaw04@gmail.com>
2020-09-10 01:26:22 +0530

    implemented crawler, implementated storing of data in sqlite3

--- webscrape1/webscrape1/pipelines.py
+++ webscrape1/webscrape1/pipelines.py
@@ -1,4 +1,29 @@
+import sqlite3
+
+
 class Webscrape1Pipeline(object):
-    def process_item(self, item, spider):
-        print("Pipeline :" + item['title'][0])
-        return item
+
+	def __init__(self):
+		self.create_connection()
+		self.create_table()
+
+	def create_connection(self):
+		self.conn = sqlite3.connect("my_quotes.db")
+		self.curr = self.conn.cursor()
+
+	def create_table(self):
+		self.curr.execute("""create table if not exists quotes_tb(
+			title text,
+			author text,
+			tags text)""" )
+
+	def process_item(self, item, spider):
+		self.store_db( item)
+		return item
+
+	def store_db(self, item):
+		self.curr.execute("""insert into quotes_tb(title, author, tags) values(?,?,?)""",(
+			item['title'][0],
+			item['author'][0],
+			item['tags'][0]) )
+		self.conn.commit()
--- webscrape1/webscrape1/spiders/quotes_spider.py
+++ webscrape1/webscrape1/spiders/quotes_spider.py
@@ -27,3 +27,8 @@ class QuotesSpider( scrapy.Spider ):
 
 			yield item
 
+		next_page = response.css("li.next a::attr(href)").get()
+
+		if( next_page != None ):
+			yield response.follow(next_page, callback = self.parse )
+
